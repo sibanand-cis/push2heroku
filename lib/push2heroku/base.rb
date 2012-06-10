5172615406d458b030ffa3d146fb89240c8fe911
Neeraj Singh <neerajdotname@gmail.com>
2012-06-10 10:00:36 -0400

    fix typo

--- lib/push2heroku/base.rb
+++ lib/push2heroku/base.rb
@@ -54,7 +54,7 @@ module Push2heroku
     end
 
     def build_config_commands
-      return unless Settings.config
+      return unless settings.config
       cmd = []
       settings.config.each do |key, value|
         cmd << "#{key.upcase}=#{value}"
